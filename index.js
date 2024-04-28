c2291754a388532e92857b3033b9cc85cbe9211f
Cicero <150862765+cicerorph@users.noreply.github.com>
2024-04-28 20:52:29 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,58 +1,52 @@
 // index.js
-const WebSocket = require('ws');
+const express = require('express');
 const DiscordRPC = require('discord-rpc');
+
+const app = express();
+const port = 6040;
 
 let rpc;
 
-console.log("Ruby Devs Technology | Made by MubiLop")
+console.log("Ruby Devs Technology | Made by MubiLop");
 
-function startWebSocketServer() {
-  const wss = new WebSocket.Server({ port: 6040 });
-  console.log("RPC: Server Started")
+app.use(express.json());
 
-  wss.on('connection', (ws) => {
-    console.log("RPC: Connected")
-    ws.on('message', (message) => {
-      try {
-        const data = JSON.parse(message);
-        if (data.applicationId && data.rpcData) {
-          updateDiscordRPC(data.applicationId, data.rpcData);
-          console.log(`RPC: ${data.rpcData}`)
-        } else {
-          console.error('Invalid WebSocket message format');
-        }
-      } catch (error) {
-        console.error('Invalid JSON format');
-      }
-    });
-  });
-
-  wss.on('close', () => {
-    console.log('WebSocket server closed. Restarting...');
-    startWebSocketServer();
-  });
-
-  function updateDiscordRPC(applicationId, rpcData) {
+function updateDiscordRPC(applicationId, rpcData) {
     if (!rpc || rpc.applicationId !== applicationId) {
-      DiscordRPC.register(applicationId);
+        DiscordRPC.register(applicationId);
 
-      rpc = new DiscordRPC.Client({ transport: 'ipc' });
-      rpc.login({ clientId: applicationId }).catch(console.error);
+        rpc = new DiscordRPC.Client({ transport: 'ipc' });
+        rpc.login({ clientId: applicationId }).catch(console.error);
     }
 
     if (rpcData.state || rpcData.details) {
-      rpc.setActivity({
-        state: rpcData.state || undefined,
-        details: rpcData.details || undefined,
-      });
+        rpc.setActivity({
+            state: rpcData.state || undefined,
+            details: rpcData.details || undefined,
+        });
     }
-  }
-
-  wss.on('upgrade', (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, (ws) => {
-      wss.emit('connection', ws, request);
-    });
-  });
 }
 
-startWebSocketServer();
+app.post('/rpc/change', (req, res) => {
+    try {
+        const { applicationId, rpcData } = req.body;
+        if (applicationId && rpcData) {
+            updateDiscordRPC(applicationId, rpcData);
+            console.log(`RPC: ${rpcData}`);
+            res.status(200).send('RPC updated successfully.');
+        } else {
+            throw new Error('Invalid request body format');
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send('Bad request.');
+    }
+});
+
+app.get('/rpc/ping', (req, res) => {
+    res.status(200).send('Pong!');
+});
+
+app.listen(port, () => {
+    console.log(`RPC server listening at http://localhost:${port}`);
+});
