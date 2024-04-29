2f09597ee50c74d1db81823debc8d68ac812b7f4
Cicero <150862765+cicerorph@users.noreply.github.com>
2024-04-28 21:04:14 -0300

    Atualizar o test.js

--- test.js
+++ test.js
@@ -2,17 +2,15 @@
 const request = require('supertest');
 const app = require('./index');
 
-describe('Testing Discord RPC endpoints', () => {
-    it('should respond with "Pong!" when hitting /rpc/ping', async () => {
-        const res = await request(app).get('/rpc/ping');
-        expect(res.statusCode).toEqual(200);
-        expect(res.text).toEqual('Pong!');
-    });
+test('should respond with "Pong!" when hitting /rpc/ping', async () => {
+    const res = await request(app).get('/rpc/ping');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual('Pong!');
+});
 
-    it('should handle invalid data when hitting /rpc/change', async () => {
-        const res = await request(app)
-            .post('/rpc/change')
-            .send({ invalidKey: 'invalidData' });
-        expect(res.statusCode).toEqual(400);
-    });
+test('should handle invalid data when hitting /rpc/change', async () => {
+    const res = await request(app)
+        .post('/rpc/change')
+        .send({ invalidKey: 'invalidData' });
+    expect(res.statusCode).toEqual(400);
 });
